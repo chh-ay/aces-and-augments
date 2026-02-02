d321d4157b95219ad457c4594365ff9cacee566b
chh-ay <vinchhaysea30@gmail.com>
2026-02-02 22:33:51 +0700

    feat: player movement

--- scripts/player/player_controller.gd
+++ scripts/player/player_controller.gd
@@ -1,2 +1,10 @@
 class_name PlayerController
 extends CharacterBody2D
+
+@export var move_speed: float = 200.0
+
+func _physics_process(_delta: float) -> void:
+	var input_vector: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_vector * move_speed
+	move_and_slide()
+
