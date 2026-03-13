fa17d15206f78e4a4c81f1c3f182ba661c5e8a5e
chh-ay <vinchhaysea30@gmail.com>
2026-03-13 20:42:15 +0700

    fix: prevent instant exit on boss death

--- scripts/world/exit_door.gd
+++ scripts/world/exit_door.gd
@@ -10,6 +10,7 @@ signal player_exited
 @onready var _panel: Panel = $CanvasLayer/PromptPanel
 
 var _activated: bool = false
+var _awaiting_player_clearance: bool = true
 
 func _ready() -> void:
 	_panel.visible = false
@@ -22,18 +23,33 @@ func _ready() -> void:
 		_trigger.collision_mask = 1
 		_trigger.body_entered.connect(_on_body_entered)
 		_trigger.body_exited.connect(_on_body_exited)
+	call_deferred("_sync_initial_clearance")
 
 func _on_body_entered(body: Node) -> void:
 	if _activated:
 		return
 	if body is PlayerController:
+		if _awaiting_player_clearance:
+			return
 		_activated = true
 		_panel.visible = true
 		call_deferred("_emit_player_exited")
 
 func _on_body_exited(body: Node) -> void:
 	if body is PlayerController:
+		if _awaiting_player_clearance:
+			_awaiting_player_clearance = false
 		_panel.visible = false
 
 func _emit_player_exited() -> void:
 	player_exited.emit()
+
+func _sync_initial_clearance() -> void:
+	if _trigger == null:
+		_awaiting_player_clearance = false
+		return
+	for body in _trigger.get_overlapping_bodies():
+		if body is PlayerController:
+			_awaiting_player_clearance = true
+			return
+	_awaiting_player_clearance = false
