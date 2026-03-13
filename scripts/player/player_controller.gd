01b6a376d4acda6fab793456e33fb1a984331b6e
chh-ay <vinchhaysea30@gmail.com>
2026-03-14 02:25:29 +0700

    fix: tighten fullscreen gameplay zoom

--- scripts/player/player_controller.gd
+++ scripts/player/player_controller.gd
@@ -107,7 +107,7 @@ enum AimMode {
 @export_range(0.1, 1.0, 0.05) var upper_terrain_move_multiplier: float = 0.72
 @export var aim_mode: int = AimMode.AUTO
 @export var manual_aim_deadzone: float = 10.0
-@export_range(0.5, 1.5, 0.05) var camera_zoom_scale: float = 0.80
+@export_range(0.5, 1.5, 0.05) var camera_zoom_scale: float = 0.65
 
 var current_health: int = 0
 var current_experience: int = 0
